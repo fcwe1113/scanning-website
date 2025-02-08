70adb83d84852bbad7fb247e04f3950f3d0a7084
fcwe1113 <fcwe1113@gmail.com>
2025-02-08 18:39:21 +0000

    tried and failed to jump pages

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,28 @@ import CircularIndeterminate from './Loading_screen';
 import { ScreenState } from './Screen_state';
 import { token_exchange } from './Token_exchange';
 
+export class referenceObj {
+  value: any = ScreenState.Loading;
+
+  constructor(value: any){
+    this.value = value
+  }
+}
+
+const StartScreen = (
+  <><h1>Welcome to the Scanning app</h1><input placeholder='username'></input><input placeholder='password'></input><div>
+    <button>Sign in</button><br />
+    <button>Sign up</button><br />
+    <button>Proceed as guest</button>
+  </div></>
+)
+
+let changeScreen: referenceObj = new referenceObj(false)
+let screen: referenceObj = new referenceObj(ScreenState.Loading)
+let page: referenceObj = new referenceObj(CircularIndeterminate())
+let token = new String("-1")
+
 function App() {
-  const [count, setCount] = useState(0)
-  const connection = useRef(null)
-  let token = new String("-1")
-  let screen: ScreenState = ScreenState.Loading // when printing we get the number
 
   // for the camera lib use the one demoed in kybarg.github.io/react-qr-scanner/
 
@@ -34,120 +51,100 @@ function App() {
       // after reading the first digit get rid of it and pass the rest of the message into the relevant function
       // messages sent from both ends should follow a similar format (at least for the first few chars)
       // *** denotes client side tasks
-        // 0 = token exchange
-        // 1 = start screen
-        // a. check items: token
-          // b. do regular status checks until user either clicks log in sign up or proceed as guest***
-          // c. if user logs in client sends username and password in textbox***
-          // with the format "1username password"
-            // I. server querys db to get password of username
-            // II. server saves username locally and pings down OK if correct
-              // if db returns incorrect or empty pings down BADINFO and returns to step 1b.
-            // III. client saves the username locally and pings "1NEXT 3 token" to server***, server go to step 1f.
-          // d. if user clicks sign up client pings "1NEXT 2 token"***, server go to step 1f.
-          // e. if user clicks proceed as guest client pings "1guest 00000000" to server***
-            // I. server saves the username locally and pings "1ACK" to client
-            // II. client saves username locally and pings "1NEXT 3 token"***, server go to step 1f.
-          // f. server decipher the message, checks the token to be correct,
-          // and extract the destination screen status contained in it
-          // g. server pings "1NEXT *2/3*" depending on which one the client sent before
-          // and server moves on to that state
-          // h. client receives message and also moves on to the next state
-        // 2 = sign up screen
-        // 3 = store locator
-        // 4 = main app (the scanning screen)
-        // 5 = payment screen
-        // 6 = transferring to till (either by choice or to check id)
-        // 7 = after payment/logging out
+      // 0 = token exchange
+      // 1 = start screen
+      // a. check items: token
+      // b. do regular status checks until user either clicks log in sign up or proceed as guest***
+      // c. if user logs in client sends username and password in textbox***
+      // with the format "1username password"
+      // I. server querys db to get password of username
+      // II. server saves username locally and pings down OK if correct
+      // if db returns incorrect or empty pings down BADINFO and returns to step 1b.
+      // III. client saves the username locally and pings "1NEXT 3 token" to server***, server go to step 1f.
+      // d. if user clicks sign up client pings "1NEXT 2 token"***, server go to step 1f.
+      // e. if user clicks proceed as guest client pings "1guest 00000000" to server***
+      // I. server saves the username locally and pings "1ACK" to client
+      // II. client saves username locally and pings "1NEXT 3 token"***, server go to step 1f.
+      // f. server decipher the message, checks the token to be correct,
+      // and extract the destination screen status contained in it
+      // g. server pings "1NEXT *2/3*" depending on which one the client sent before
+      // and server moves on to that state
+      // h. client receives message and also moves on to the next state
+      // 2 = sign up screen
+      // 3 = store locator
+      // 4 = main app (the scanning screen)
+      // 5 = payment screen
+      // 6 = transferring to till (either by choice or to check id)
+      // 7 = after payment/logging out
 
       let response = evt.data
       let oprand = response.slice(0, 1)
       response = response.replace(oprand, "")
       switch (oprand) {
         case "0":
-          token_exchange(socket, response, screen, token)
+          changeScreen.value = token_exchange(socket, response, screen, token)
+          console.debug("screen state: " + screen.value)
+      }
+
+      if (screen.value == ScreenState.Loading) {
+        page.value = CircularIndeterminate()
+      } else if (screen.value == ScreenState.Start) {
+        page.value = StartScreen
       }
     };
 
-    function send(socket: WebSocket, msg: String){
-      socket.addEventListener("open", event => {
-        console.log("hi")
-        socket.send(msg as string)
-      });
-    }
-    
-      // socket.addEventListener("open", event => {
-      //   // console.log("hi")
-      //   socket.send("1Connection established from "/*new Blob(["Connection established from " + id])*/)
-      // });
 
-      return () => socket.close()
+
+    // function send(socket: WebSocket, msg: String){
+    //   socket.addEventListener("open", event => {
+    //     console.log("hi")
+    //     socket.send(msg as string)
+    //   });
+    // }
+
+    // socket.addEventListener("open", event => {
+    //   // console.log("hi")
+    //   socket.send("1Connection established from "/*new Blob(["Connection established from " + id])*/)
+    // });
+
+    return () => socket.close()
   }, [])
-  
 
-  
-  //for some reason the html returns hv to be packaged with empty tags like <></> 
-  if (screen == ScreenState.Loading) {
-    return CircularIndeterminate()
-  } else {
-    return (
-      // StartScreen
-      <>
-        {/* <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box> */}
-        <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-
-      </>
-
-    )
+  if (changeScreen.value) {
+    console.log("used screen: " + screen.value)
+    if (screen.value == ScreenState.Loading) {
+      page.value = CircularIndeterminate()
+    } else if (screen.value == ScreenState.Start) {
+      page.value = StartScreen
+    }
+    changeScreen.value = false
+    // return page.value
   }
+
+  return(
+    <>
+    {page.value}
+    </>
+  )
+  
+
+  // return () => {
+  //   var page = (<><h1>dummy page</h1></>)
+  //   return useEffect(() => {
+  //     console.log("used screen: " + screen.value)
+  //     if (screen.value == ScreenState.Loading) {
+  //       page = CircularIndeterminate()
+  //     } else if (screen.value == ScreenState.Start) {
+  //       page = StartScreen
+  //     }
+  //   }, [screen.value])
+  //   return page
+  // }
+
+
 }
 
-const StartScreen = (
-  <>
-    <h1>Welcome to the Scanning app</h1>
-    <input placeholder='username'></input>
-    <input placeholder='password'></input>
-    <div>
-      <button>Sign in</button><br />
-      <button>Sign up</button><br />
-      <button>Proceed as guest</button>
-    </div>
-  </>
-)
+
 
 
 export default App
--- src/Token_exchange.tsx
+++ src/Token_exchange.tsx
@@ -1,29 +1,38 @@
+import { referenceObj } from "./App"
 import { ScreenState } from "./Screen_state"
 
 // messages sent from both ends should follow a similar format (at least for the first few chars)
 // *** denotes client side tasks
 // 0 = token exchange
-    // a. when the websocket channel opens the server will generate the token and send it to the client, the client would have a placeholder token which prevent the client from proceeding
-    // b. the client saves the token and pings back the same token to the server***
-    // c. the server sends an ack back if the token matches and switches the token_exchanged flag on and saves it in the list(tm)
-    // d. client then tells the server to move on to the start screen state***
-    // e. server tells client to move on then moves on itself, unless the token_exchanged flag is not on, in which case handle the error
-    // f. client moves on for real***
-    // while the client is waiting for the token exchange ack it can show a loading wheel or something idk
+// a. when the websocket channel opens the server will generate the token and send it to the client, the client would have a placeholder token which prevent the client from proceeding
+// b. the client saves the token and pings back the same token to the server***
+// c. the server sends an ack back if the token matches and switches the token_exchanged flag on and saves it in the list(tm)
+// d. client then tells the server to move on to the start screen state***
+// e. server tells client to move on then moves on itself, unless the token_exchanged flag is not on, in which case handle the error
+// f. client moves on for real***
+// while the client is waiting for the token exchange ack it can show a loading wheel or something idk
 
 // as all parameters passed in are objects (yes they are trust me) they are all passed by reference
-export function token_exchange(socket: WebSocket, response: String, screen: ScreenState, token: String) {
+export function token_exchange(socket: WebSocket, response: String, screen: referenceObj, token: String) : boolean {
     if (response == "ACK") {
         socket.send("0NEXT") // 0d request next screen
+        console.info("sent \"0NEXT\"")
+        return false
     } else if (response == "NEXT") {
-        screen = ScreenState.Start // 0f moving on
-        console.log("yes it worked")
+        // console.log("b4: " + screen)
+        screen.value = ScreenState.Start // 0f moving on
+        // console.log("after: " + screen)
+        // console.log("yes it worked")
+        return true
     } else if (response.length == 10) {
         token = response // 0b save token
         // console.log(token)
         socket.send("0" + token) // 0b send it back
+        console.info("sent \"0" + token + "\"")
+        return false
     } else { // this will only happen if i fked up the backend
         console.log(response)
         console.error("wtf did i just receive")
+        return false
     }
 }
